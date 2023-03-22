dd7440cbb1294d8d395f9e46d8eca72944ab02e1
fox <12120644+foxydevloper@users.noreply.github.com>
2023-03-22 05:34:33 -0400

    pretty

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
-export { Buffer, readLines } from "https://deno.land/std@0.180.0/io/mod.ts";
+export { Buffer } from "https://deno.land/std@0.180.0/io/buffer.ts";
+export { readLines } from "https://deno.land/std@0.180.0/io/read_lines.ts";
